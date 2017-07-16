af596cce4cbc4d7145dfc974e568800ef27ddf88
asnr <asnr@users.noreply.github.com>
2017-07-17 06:54:58 +1000

    Finish set 1 challenge 7

--- set1/challenge7.py
+++ set1/challenge7.py
@@ -1,5 +1,19 @@
+import codecs
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
 with open('7.txt') as fp:
     base64_encoded_ciphertext_lines = fp.readlines()
 
-base64_encoded_ciphertext =
-  ''.join(line.strip() for line in base64_encoded_ciphertext_lines)
+base64_encoded_ciphertext = ''.join(
+        line.strip() for line in base64_encoded_ciphertext_lines)
+
+ciphertext = codecs.decode(base64_encoded_ciphertext, 'base64')
+
+backend = default_backend()
+key = 'YELLOW SUBMARINE'
+cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+decryptor = cipher.decryptor()
+plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+print(plaintext)
